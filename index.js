088efd2706ec463384cb52d09c21df78b2f51fe2
Adriaan Pelzer <adriaan@wewillraakyou.com>
2020-11-27 17:48:16 +0000

    fix: do not double-encapsulate request, and transform lambda response

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ const buildHeaders = headers => Object.keys(headers).reduce((hdrs, key) => ({
   [key.toLowerCase()]: [{ key: capitalise(key), value: headers[key].toString() }]
 }), {});
 
-const buildRequest = ({ headers = {}, originHeaders = {}, ...request }) => ({
-  Records: [{ cf: { request: {
-    ...requonse,
-    headers: buildHeaders(headers),
+const buildRequest = ({ method = 'GET', headers = {}, originHeaders, ...request }) => ({
+  method,
+  ...request,
+  headers: buildHeaders(headers),
+  ...(originHeaders ? {
     origin: { custom: { customHeaders: buildHeaders(originHeaders) } }
-  } } }]
+  } : {})
 });
 
 const buildEvent = ({ request = {}, response }) => ({ Records: [{ cf: {
@@ -25,4 +26,11 @@ const buildEvent = ({ request = {}, response }) => ({ Records: [{ cf: {
 
 module.exports = ({ module, handler, request, response, context }) => awsLambda({
   module, handler, event: buildEvent({ request, response, context })
-});
+})
+  .then(response => ({
+    ...response,
+    headers: Object.keys(response.headers).reduce((headers, key) => ({
+      ...headers,
+      [response.headers[key][0].key]: response.headers[key][0].value
+    }), {})
+  }));
